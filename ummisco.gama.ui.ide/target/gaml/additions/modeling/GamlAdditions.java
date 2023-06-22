a2d2e281bf44f9651b250bab7c4f74aa5f9fb8a8
hqnghi88 <hqnghi88@gmail.com>
2023-06-22 10:43:27 +0700

    test

---
+++ ummisco.gama.ui.ide/target/gaml/additions/modeling/GamlAdditions.java
@@ -0,0 +1,58 @@
+package gaml.additions.modeling;
+
+import msi.gama.util.matrix.*;
+import msi.gama.util.graph.*;
+import msi.gama.metamodel.shape.*;
+import java.lang.*;
+import msi.gama.util.*;
+import msi.gama.util.path.*;
+import msi.gama.extensions.messaging.*;
+import msi.gaml.compilation.*;
+import msi.gama.util.file.*;
+import msi.gaml.architecture.finite_state_machine.*;
+import msi.gama.common.interfaces.*;
+import msi.gaml.architecture.user.*;
+import msi.gama.kernel.simulation.*;
+import msi.gama.outputs.*;
+import msi.gaml.species.*;
+import msi.gaml.architecture.weighted_tasks.*;
+import msi.gama.kernel.experiment.*;
+import  msi.gama.metamodel.shape.*;
+import msi.gaml.extensions.multi_criteria.*;
+import msi.gaml.types.*;
+import msi.gama.kernel.batch.*;
+import msi.gama.kernel.root.*;
+import java.util.*;
+import msi.gaml.factories.*;
+import msi.gaml.skills.*;
+import msi.gama.util.tree.*;
+import msi.gama.outputs.layers.*;
+import msi.gama.runtime.*;
+import msi.gaml.statements.test.*;
+import msi.gama.metamodel.agent.*;
+import msi.gaml.expressions.*;
+import msi.gama.metamodel.topology.*;
+import msi.gaml.statements.draw.*;
+import msi.gaml.descriptions.*;
+import msi.gaml.variables.*;
+import msi.gaml.operators.*;
+import msi.gama.runtime.exceptions.*;
+import msi.gaml.statements.*;
+import msi.gama.kernel.model.*;
+import msi.gama.metamodel.population.*;
+import msi.gama.outputs.layers.charts.*;
+import msi.gaml.architecture.reflex.*;
+import msi.gaml.operators.Random;
+import msi.gaml.operators.Maths;
+import msi.gaml.operators.Points;
+import msi.gaml.operators.Spatial.Properties;
+import msi.gaml.operators.System;
+import static msi.gaml.operators.Cast.*;
+import static msi.gaml.operators.Spatial.*;
+import static msi.gama.common.interfaces.IKeyword.*;
+@SuppressWarnings({ "rawtypes", "unchecked", "unused" })
+
+public class GamlAdditions extends msi.gaml.compilation.AbstractGamlAdditions {
+	public void initialize() throws SecurityException, NoSuchMethodException {
+}
+}
